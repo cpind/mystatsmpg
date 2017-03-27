78365196b176c8633604c073fa03aaf102c21f17
cpind <cyprien.pindat@gmail.com>
2017-03-27 18:42:25 -0400

    adds package definitions

---
+++ pystatsmpg/__init__.py
@@ -0,0 +1,19 @@
+import store
+
+
+def update(stats):
+    if type(stats) is str:
+        if stats[:-4] == "xlsx":
+            return store.update_xlsx(stats)
+        if stats[:-4] == "csv":
+            return store.update(csv = stats)
+    if type(stats) is dict:
+        return store.update(players=stats['players'], teams=stats['teams'])
+
+
+def clear():
+    return store.clear()
+
+
+def dump():
+    return store.dump()
